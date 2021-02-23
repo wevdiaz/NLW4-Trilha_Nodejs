9b33fc5fa18ccf7df968b5f8f57f327474368dea
Weverson Diazz <wevers.info@gmail.com>
2021-02-23 20:26:51 -0300

    Update server.ts
    configurando para exibição de arquivo json.
    
    Pegando o arquivo de rotas externa.

--- API/src/server.ts
+++ API/src/server.ts
@@ -1,16 +1,12 @@
 import 'reflect-metadata';
 import express from 'express';
 import "./database";
+import { router } from './routes/routes';
 
 const app = express();
 
-app.get("/", (req, res) => {
-    return res.json({message: "NLW04 - Trilha Nodejs"})
-});
-
-app.post("/", (req, res) => {
-    return res.json({ message: "Os dados foram salvos com sucesso!"})
-});
+app.use(express.json());
+app.use(router);
 
 app.listen(3333, () => console.log("Server is working!"));
 
