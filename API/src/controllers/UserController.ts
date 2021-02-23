b62ab3969791059ae239dfc54712fedec916e7fd
Weverson Diazz <wevers.info@gmail.com>
2021-02-23 20:25:25 -0300

    Create UserController.ts
    Criando o Controller para usuários

---
+++ API/src/controllers/UserController.ts
@@ -0,0 +1,13 @@
+import { Request, Response } from "express";
+
+class UserController {
+
+    async create(req: Request, res: Response) {
+        const body = req.body;
+        console.log(body)
+
+        return res.send();
+    }
+}
+
+export { UserController };
