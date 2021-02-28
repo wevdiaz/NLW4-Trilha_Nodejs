7f6c41c732184c5edeb0386e242983d44b317b1e
Weverson Diazz <wevers.info@gmail.com>
2021-02-27 22:17:18 -0300

    Exibindo relação de tabelas
    Exibindo os dados da relação entre tabelas.

--- API/src/controllers/SendMailController.ts
+++ API/src/controllers/SendMailController.ts
@@ -41,7 +41,8 @@ class SendMailController {
         }
 
         const surveyUserAlreadyExists = await surveysUsersRepository.findOne({
-            where: [{ user_id: user.id }, { value: null} ]
+            where: [{ user_id: user.id }, { value: null} ],
+            relations: ["user", "survey"]
         });
 
         if (surveyUserAlreadyExists) {
