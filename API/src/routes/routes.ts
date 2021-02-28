6f7c544bd41378b8ed46beafd30d2d61205d9859
Weverson Diazz <wevers.info@gmail.com>
2021-02-28 20:06:31 -0300

    Create AnswerController.ts
    Criando o controller de resposta, pegando o valor escolhido pelo usuário

--- API/src/routes/routes.ts
+++ API/src/routes/routes.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { AnswerController } from "../controllers/AnswerController";
 import { SendMailController } from "../controllers/SendMailController";
 import { SurveysController } from "../controllers/SurveysController";
 import { UserController } from "../controllers/UserController";
@@ -10,6 +11,7 @@ const userController = new UserController();
 const surveysController = new SurveysController();
 
 const sendMailController = new SendMailController();
+const answerController = new AnswerController();
 
 router.post("/users", userController.create );
 
@@ -17,5 +19,7 @@ router.post("/surveys", surveysController.create );
 router.get("/surveys", surveysController.show );
 
 router.post("/sendMail", sendMailController.execute );
+
+router.get("/answers/:value", answerController.execute );
 
 export { router };
