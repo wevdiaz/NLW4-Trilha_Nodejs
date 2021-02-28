d657b63e36ee0a8f4816f8663ea0b436ce4d4503
Weverson Diazz <wevers.info@gmail.com>
2021-02-28 19:40:05 -0300

    Refatorando SendMailController.ts
    Organizando o código

--- API/src/controllers/SendMailController.ts
+++ API/src/controllers/SendMailController.ts
@@ -30,22 +30,23 @@ class SendMailController {
             });
         }
 
-        const npsPath = resolve(__dirname, "..", "views", "emails", "npsMail.hbs");
+        const npsPath = resolve(__dirname, "..", "views", "emails", "npsMail.hbs");       
+
+        const surveyUserAlreadyExists = await surveysUsersRepository.findOne({
+            where: { user_id: user.id, value: null},
+            relations: ["user", "survey"]
+        });
 
         const variables = {
             name: user.name,
             title: survey.title,
             description: survey.description,
-            user_id: user.id,
+            id: "",
             link: process.env.URL_MAIL
         }
 
-        const surveyUserAlreadyExists = await surveysUsersRepository.findOne({
-            where: [{ user_id: user.id }, { value: null} ],
-            relations: ["user", "survey"]
-        });
-
         if (surveyUserAlreadyExists) {
+            variables.id = surveyUserAlreadyExists.id;
             await SendMailService.execute(email, survey.title, variables, npsPath);
             return res.json(surveyUserAlreadyExists);
         }
@@ -55,7 +56,9 @@ class SendMailController {
             survey_id
         });
 
-        await surveysUsersRepository.save(surveyUser);        
+        await surveysUsersRepository.save(surveyUser);
+        
+        variables.id = surveyUser.id;
 
         await SendMailService.execute(email, survey.title, variables, npsPath);
 
