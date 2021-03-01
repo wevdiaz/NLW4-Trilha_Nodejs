30d88f3b73ae40e01272f0ee5753e7dd2a65bf34
Weverson Diazz <wevers.info@gmail.com>
2021-02-28 22:19:42 -0300

    Update UserController.ts
    Criando uma validação para usuário

--- API/src/controllers/UserController.ts
+++ API/src/controllers/UserController.ts
@@ -1,11 +1,24 @@
 import { Request, Response } from "express";
 import { getCustomRepository } from "typeorm";
 import { UsersRepository } from "../repositories/UsersRepository";
+import * as yup from "yup";
 
 class UserController {
 
     async create(req: Request, res: Response) {
         const { name, email } = req.body;
+
+        const schema = yup.object().shape({
+            name: yup.string().required(),
+            email: yup.string().email().required()
+        });
+
+       
+        try {
+            await schema.validate(req.body);
+        }catch(err) {
+            return res.status(400).json({ error: err });
+        }
 
         const usersRepository = getCustomRepository(UsersRepository);
 
