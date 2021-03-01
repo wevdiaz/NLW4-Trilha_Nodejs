7b78cec731f21fdf090e35f761e887e6788a537a
Weverson Diazz <wevers.info@gmail.com>
2021-03-01 00:12:23 -0300

    Tratamento de erros
    Fazendo tratamento de erros, usando o throw .

--- API/src/controllers/AnswerController.ts
+++ API/src/controllers/AnswerController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import { getCustomRepository } from "typeorm";
 import { SurveysUsersRepository } from "../repositories/SurveysUsersRepository";
+import { AppError } from "../errors/AppError";
 
 class AnswerController {
 
@@ -15,9 +16,7 @@ class AnswerController {
         });
 
         if(!surveyUser) {
-            return res.status(400).json({
-                error: "Survey User does not exists!"
-            });
+            throw new AppError("Survey User does not exists!");           
         }
 
         surveyUser.value = Number(value);
--- API/src/controllers/UserController.ts
+++ API/src/controllers/UserController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import { getCustomRepository } from "typeorm";
 import { UsersRepository } from "../repositories/UsersRepository";
 import * as yup from "yup";
+import { AppError } from "../errors/AppError";
 
 class UserController {
 
@@ -17,7 +18,7 @@ class UserController {
         try {
             await schema.validate(req.body);
         }catch(err) {
-            return res.status(400).json({ error: err });
+            throw new AppError(err); 
         }
 
         const usersRepository = getCustomRepository(UsersRepository);
@@ -27,9 +28,7 @@ class UserController {
         });
 
         if (userAlreadyExists) {
-            return res.status(400).json({
-                error: "User already exists!"
-            })
+            throw new AppError("User already exists!");            
         }
         
         const user = usersRepository.create({
